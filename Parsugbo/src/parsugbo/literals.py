f4598b45a81bfbc78eaff123db8cc651a7a34417
Sweep76 <joshuapatrickchiu76@gmail.com>
2025-09-18 19:33:06 +0800

    feat: add literals for Cebuano language processing

---
+++ Parsugbo/src/parsugbo/literals.py
@@ -0,0 +1,101 @@
+LITERALS_PRESENT_PREFIX = ["nag", "na", "gi", "i", "naga", "nang"]
+LITERALS_PAST_PREFIX = ["ni", "nag", "naka", "na", "gi", "naga", "nang"]
+LITERALS_FUTURE_PREFIX = ["mo", "mag", "maka", "ma", "i", "maga", "mang"]
+LITERALS_PRESENT_SUFFIX = ["mo", "mag", "maka", "ma", "i"]
+LITERALS_PAST_SUFFIX = ["an"]
+LITERALS_MONTHS = [
+    "enero",
+    "pebrero",
+    "marso",
+    "abril",
+    "mayo",
+    "hunyo",
+    "hulyo",
+    "agosto",
+    "septiyembre",
+    "oktubre",
+    "nubiyembre",
+    "disyembre",
+]
+LITERALS_HOURS = [
+    "uno",
+    "dos",
+    "tres",
+    "kwatro",
+    "singko",
+    "sais",
+    "siete",
+    "otso",
+    "nuwebe",
+    "diyes",
+    "onse",
+    "dose",
+]
+LITERALS_TIMES_OF_DAY = ["ka-adlawon", "buntag", "udto", "hapon" "gabi-i"]
+LITERALS_FUTURE_SUFFIX = ["on", "hon", "an"]
+LITERALS_INTER = [
+    "unsa",
+    "kinsa",
+    "ngano",
+    "kanusa",
+    "asa",
+    "hain",
+    "diin",
+    "kon",
+    "samtang",
+    "pila",
+    "ginusa",
+    "unsaon",
+    "tagpila",
+]
+LITERALS_PREPS = [
+    "sa",
+    "kang",
+    "kay",
+    "batok",
+    "de",
+    "gawas",
+    "gikan",
+    "imbes",
+    "kapin",
+    "minos",
+    "mura",
+    "ngadto",
+    "nganha",
+    "para",
+    "puyra",
+    "pwera",
+    "sulod",
+    "supak",
+    "tungod",
+    "tupad",
+    "uban",
+    "ubos",
+]
+LITERALS_TIME_NOUNS = ["adlaw", "tuig", "semana"]
+LITERALS_TIME_NOUNS_A = ["adlawa", "tuiga", "semanaha"]
+LITERALS_TIMES = ["kagahapon", "karon", "ugma", "kaganina"]
+LITERALS_PRON_DEMS = [
+    "kiri",
+    "kini",
+    "kana",
+    "kadto",
+    "niiri",
+    "niini",
+    "niani",
+    "niari",
+    "niana",
+    "niadto",
+]
+LITERALS_PLURAL_DETS = ["sina", "mga"]
+LITERALS_DETS = ["sa"]
+LITERALS_POS_LINKS = ["ni", "sa"]
+LITERALS_PRON_POS_SINGS = ["ako", "imo", "iya", "nako", "nimo", "niya"]
+LITERALS_PRON_POS_N_SINGS = ["nako", "nimo", "niya"]
+LITERALS_PRON_PER_SINGS = ["ikaw", "ka", "siya", "ko"] + LITERALS_PRON_POS_SINGS
+LITERALS_PRON_POS_PLURALS = ["amo", "ato", "inyo", "ila", "namo", "nato", "ninyo", "nila"]
+LITERALS_PRON_POS_N_PLURALS = ["namo", "nato", "ninyo", "nila"]
+LITERALS_PRON_PER_PLURALS = ["kami", "kita", "kamo", "sila", "ta", "mi"] + LITERALS_PRON_POS_PLURALS
+LITERALS_PRON_POS_SINGS_NG = [pro + "ng" for pro in LITERALS_PRON_POS_SINGS]
+LITERALS_ADV_SPECS = ["na", "pa", "man", "ba"]
+LITERALS_PRON_POS_PLURALS_NG = [pro + "ng" for pro in LITERALS_PRON_POS_PLURALS]
